6d1ea2bdd92cec6b7a4fdb1a1818a3cb6c32e069
Julius Krah <juliuskrah@gmail.com>
2016-07-16 12:47:08 +0000

    added 'mongobee' changelog migration tool

--- src/main/java/com/jipasoft/config/MongoConfig.java
+++ src/main/java/com/jipasoft/config/MongoConfig.java
@@ -19,15 +19,25 @@ import static com.jipasoft.domain.util.JSR310DateConverters.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.mongodb.core.convert.CustomConversions;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import com.github.mongobee.Mongobee;
 import com.jipasoft.repository.mongo.BaseRepositoryImpl;
 import com.jipasoft.util.Profiles;
+import com.mongodb.Mongo;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Configuration specific for {@code mongo} profile. This configuration uses
@@ -36,11 +46,17 @@ import com.jipasoft.util.Profiles;
  * @author Julius Krah
  *
  */
+@Slf4j
 @Profile(Profiles.MONGO)
 @Configuration
+@Import(value = MongoAutoConfiguration.class)
 @EnableMongoRepositories(basePackageClasses = BaseRepositoryImpl.class)
 public class MongoConfig {
-	
+	@Autowired
+	private MongoProperties properties;
+	@Inject
+	private Mongo mongo;
+
 	@Bean
 	public CustomConversions customConversions() {
 		List<Converter<?, ?>> converters = new ArrayList<>();
@@ -51,5 +67,16 @@ public class MongoConfig {
 		converters.add(DateToLocalDateTimeConverter.INSTANCE);
 		converters.add(LocalDateTimeToDateConverter.INSTANCE);
 		return new CustomConversions(converters);
+	}
+
+	@Bean
+	public Mongobee mongobee() {
+		log.debug("Configuring Mongobee");
+		Mongobee mongobee = new Mongobee(mongo);
+		mongobee.setDbName(properties.getDatabase());
+		// package to scan for migrations
+		mongobee.setChangeLogsScanPackage("com.jipasoft.config.dbmigrations");
+		mongobee.setEnabled(true);
+		return mongobee;
 	}
 }

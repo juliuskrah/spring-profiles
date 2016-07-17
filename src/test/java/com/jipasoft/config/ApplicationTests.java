077e6ba727b07a1d601c40a53c71ce67359d7e15
Julius Krah <juliuskrah@gmail.com>
2016-07-17 02:24:35 +0000

    fixed tests

--- src/test/java/com/jipasoft/config/ApplicationTests.java
+++ src/test/java/com/jipasoft/config/ApplicationTests.java
@@ -15,6 +15,7 @@
 */
 package com.jipasoft.config;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -25,4 +26,8 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @WebAppConfiguration
 public class ApplicationTests {
 
+	@Test
+	public void contextsLoad() {
+
+	}
 }
